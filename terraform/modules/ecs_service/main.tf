7619d4a858d1097dd3b6e9f20ca7ac36bda6ff15
Faizan <Faizan.ushahid@gmail.com>
2024-11-30 15:23:10 +0500

    ALB and API Gateway merged

--- terraform/modules/ecs_service/main.tf
+++ terraform/modules/ecs_service/main.tf
@@ -197,3 +197,8 @@ resource "aws_lb_listener_rule" "upload_photo_rule" {
     target_group_arn = var.upload_photo_lambda_target_group_arn
   }
   }
+
+  output "alb_dns_name" {
+    value = aws_lb.ecs_alb.dns_name
+    description = "DNS name of the Application Load Balancer"
+}
